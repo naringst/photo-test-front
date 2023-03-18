f3d51c29e3a9112c8c6d7079da793b9752aa494b
Ellie <naringst@gmail.com>
2023-03-19 01:42:24 +0900

    feat : 사진 필터링 및 리프레쉬 되도록 구현

--- apps/nari-vanila/src/index.js
+++ apps/nari-vanila/src/index.js
@@ -1,27 +1,54 @@
-const filterParams = {
-    filter : 'cat'
-}
-async function  fetchPhotos() {
-const result =  await fetch('/api/photos').then(res => res.json())
-// const filterValue = getFilterValue();
-// const filteredPhoto = result.filter(data => data.filter == filterValue)
+// const filterLists = ['cat','dog'];
 
-result.map(each =>  showPhotos(each.src.tiny))     
-}
+// function getFilterValue() {
+//     const filterNode = document.getElementsByName('filter')
+//     filterNode.forEach((node) => {
+//     if (node.checked){
+//         console.log(node.value)
+//         return node.value     
+//     }})
+// }
+   
+async function fetchPhotos(value) {
+    //여기 어떻께 해야 잘하쥐 ...
+
+    const url = '/api/photos'
+    if (value == 'cat') {
+        requrl = '/api/photos?filter=cat'
+    
+    }else if(value == 'dog') {
+        requrl = '/api/photos?filter=dog'
+    }
+    else {
+        requrl = url
+    }
+
+    //사진 렌더링
+    const result =  await fetch(requrl).then(res => res.json())
+    result.map(each => showPhotos(each.src.tiny));
+
+    //선택값 변경시 리프레쉬 되도록
+    var refresher = document.querySelectorAll('li');
+    refresher.forEach(function() {
+        addEventListener('click' , refreshPhotos)
+    
+    })
+       
+    
+} 
+
 
 function showPhotos(url){
-    Image = document.createElement('img');
-    Image.src = url
-    document.body.appendChild(Image);
+    const photoDiv = document.getElementById('photos');
+    newImage = document.createElement('img');
+    newImage.src = url;
+    photoDiv.appendChild(newImage);
+
 }
 
-function getFilterValue() {
-    const filterNode = document.getElementsByName('filter')
-    filterNode.forEach((node) => {
-    if (node.checked){
-        console.log(node.value)
-        return node.value
-       
-    }
-})
+function refreshPhotos(){
+    const photoDiv = document.getElementById('photos');
+    photoDiv.innerHTML = ""
+    
 }
+
